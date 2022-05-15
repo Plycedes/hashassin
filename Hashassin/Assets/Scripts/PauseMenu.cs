d3f3a7ab5f832d59ffc52d2e73b1117bc89e6321
Abhay Pratap Yadav <abhaypratapyadav340@gmail.com>
2022-05-15 23:05:53 +0530

    PauseMenu

--- Hashassin/Assets/Scripts/PauseMenu.cs
+++ Hashassin/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
     public GameObject gameUI;
 
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        gameUI.SetActive(false);
+        Time.timeScale = 0;
+        FindObjectOfType<AudioManager>().Play("menuClick");
+    }
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        gameUI.SetActive(true);
         Time.timeScale = 1;
+        FindObjectOfType<AudioManager>().Play("menuClick");
     }
     public void MainMenu()
     {
-
+        SceneManager.LoadScene(0);
+        FindObjectOfType<AudioManager>().Play("menuClick");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        FindObjectOfType<AudioManager>().Play("menuClick");
     }
 }
